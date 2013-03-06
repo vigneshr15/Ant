6b67dcd9490a9773b4a4395c79dce4ebabd34afe
RV <oracle@localbuild.aaitg.com>
2013-03-06 14:03:02 -0800

    second commit

---
+++ src/test1/Test1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author vramanathan
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is working");
+    }
+}
